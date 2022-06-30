6fdd968d65917628eb40ac870cd0638e34ef9c8c
Borja Paz Rodríguez <borjapazr@gmail.com>
2022-06-30 11:52:13 +0200

    feat(footer): add github link

---
+++ src/app/shared/modules/icons/github-icon/github-icon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-github-icon',
+  templateUrl: './github-icon.component.html',
+  styleUrls: ['./github-icon.component.scss']
+})
+export class GithubIconComponent {
+  constructor() {}
+}
--- src/app/shared/modules/icons/icons.module.ts
+++ src/app/shared/modules/icons/icons.module.ts
@@ -1,9 +1,10 @@
 import { NgModule, Type } from '@angular/core';
 
+import { GithubIconComponent } from './github-icon/github-icon.component';
 import { HappyIconComponent } from './happy-icon/happy-icon.component';
 import { HeartIconComponent } from './heart-icon/heart-icon.component';
 
-const ICONS: Type<any>[] = [HappyIconComponent, HeartIconComponent];
+const ICONS: Type<any>[] = [HappyIconComponent, HeartIconComponent, GithubIconComponent];
 
 @NgModule({
   declarations: [...ICONS],
